3e2b67bf766eeaab9e9aeae1401d2e3c9f8dc8cb
huuphat070606 <24521290@gm.uit.edu.vn>
2026-05-08 09:54:37 +0700

    Update student.py
    commit lần 2

--- student.py
+++ student.py
@@ -3,3 +3,6 @@ class Student:
         self.student_id = student_id
         self.name = name
         self.major = major
+
+    def __str__(self):
+        return f"ID: {self.student_id} | Tên: {self.name} | Ngành: {self.major}"
